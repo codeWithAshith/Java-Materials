7b79bd5e91819efe5e97997168b0ae71ab64f95f
codeWithAshith <104576296+codeWithAshith@users.noreply.github.com>
2022-09-21 13:19:39 +0530

    Add files via upload

---
+++ Nineteen.java
@@ -0,0 +1,14 @@
+public class Nineteen {
+    public static void main(String[] args) {
+        int rows = 4, number = 1;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+
+            System.out.println();
+        }
+    }
+}
